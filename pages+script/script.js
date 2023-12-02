0b9ba556947ca4bfc675473fbf8c95bcc4376815
“etcio2” <147083284+edomarte@users.noreply.github.com>
2023-12-02 17:53:16 +0000

    Edoardo no css v1.2 Stylying fixed

--- pages+script/script.js
+++ pages+script/script.js
@@ -44,10 +44,14 @@ function setEventListeners() {
 // logout(): clear the loggedInUser variable and nullifies the sessionStorage 'loggedInUser' item. Then shows an alert 'Logout successful!' and redirect to the homepage.
 
 function logout() {
-    loggedInUser = null;
-    sessionStorage.setItem('loggedInUser', null);
-    alert("Logout successful!");
-    window.location.replace("index.html");
+    if (loggedInUser != null) {
+        loggedInUser = null;
+        sessionStorage.setItem('loggedInUser', null);
+        alert("Logout successful!");
+        window.location.replace("index.html");
+    } else
+        alert("You are not logged in.");
+
 }
 
 // setParagloggedInUser(): set the value for the placeholder in the homepage to the name of the logged in user.
@@ -371,4 +375,43 @@ function updateSessionUserlist() {
 
 function updateSessionloggedInUser() {
     setItemInSessionStorage('loggedInUser', loggedInUser);
+}
+
+
+
+let items = document.querySelectorAll('.slider .item');
+let next = document.getElementById('next');
+let prev = document.getElementById('prev');
+
+let active = 1;
+function loadShow() {
+    let stt = 0;
+    items[active].style.transform = `none`;
+    items[active].style.zIndex = 1;
+    items[active].style.filter = 'none';
+    items[active].style.opacity = 1;
+    for (var i = active + 1; i < items.length; i++) {
+        stt++;
+        items[i].style.transform = `translateX(${120 * stt}px) scale(${1 - 0.2 * stt}) perspective(16px) rotateY(-1deg)`;
+        items[i].style.zIndex = -stt;
+        items[i].style.filter = 'blur(5px)';
+        items[i].style.opacity = stt > 2 ? 0 : 0.6;
+    }
+    stt = 0;
+    for (var i = active - 1; i >= 0; i--) {
+        stt++;
+        items[i].style.transform = `translateX(${-120 * stt}px) scale(${1 - 0.2 * stt}) perspective(16px) rotateY(1deg)`;
+        items[i].style.zIndex = -stt;
+        items[i].style.filter = 'blur(5px)';
+        items[i].style.opacity = stt > 2 ? 0 : 0.6;
+    }
+}
+loadShow();
+next.onclick = function () {
+    active = active + 1 < items.length ? active + 1 : active;
+    loadShow();
+}
+prev.onclick = function () {
+    active = active - 1 >= 0 ? active - 1 : active;
+    loadShow();
 }
