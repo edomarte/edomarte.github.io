4ecc635ced928dd4a72dde75ac2d8ef6662f3646
jabberjayjay <jabberjayjay@gmail.com>
2023-12-02 17:23:35 +0000

    Finalized styling and tested.

--- pages+script/script.js
+++ pages+script/script.js
@@ -1,75 +1,93 @@
-var users = {};
-var currentUser;
-var couponTypes = ["10% discount in shops", "free keychain", "5€ gift card"];
-var couponLocation;
+var users = {}; // array that contains the registred users
+var loggedInUser; // logged in user
+var couponTypes = ["10% discount in shops", "free keychain", "5€ gift card"]; // Coupons that can be extracted
+var couponLocation; // stores the position of the coupon in the current extraction
+
+// loadingPage(): runs every time a page is loaded to initialize variables, set users, assign value to the placeholder in home, and check if user already played.
 
 function loadingPage() {
     setEventListeners();
     setUsers();
-    setParagCurrentUser();
-    userAlreadyPlayed();
+    setParagloggedInUser();
+    setPlayPage();
 }
+
+// setUsers(): get users array and loggedInUser value from sessionStorage, parse them through JSON, and assign them to the respective script variables.
 
 function setUsers() {
     if (sessionStorage.getItem('users') != null)
         users = JSON.parse(sessionStorage.getItem('users'));
-    if (sessionStorage.getItem('currentUser') != null)
-        currentUser = JSON.parse(sessionStorage.getItem('currentUser'));
+    if (sessionStorage.getItem('loggedInUser') != null)
+        loggedInUser = JSON.parse(sessionStorage.getItem('loggedInUser'));
 }
 
+// setEventListeners(): set event listeners on HTML items depending on the page loaded, since each page have different components. Exception: the logout button exist in all the pages.
+
 function setEventListeners() {
+    // Signup page
     if (location.pathname.includes("signup"))
         $("#form").on("submit", validateAndRegister);
+    // Login page
     if (location.pathname.includes("login"))
         $("#form").on("submit", login);
+    // Play page
     if (location.pathname.includes("play")) {
-        $("#btnPlay").on("click", play);
+
         $("#btnChoice1").on("click", checkResult);
         $("#btnChoice2").on("click", checkResult);
         $("#btnChoice3").on("click", checkResult);
     }
+    // Button logout
     $("#btnLogout").on("click", logout);
 }
 
-
+// logout(): clear the loggedInUser variable and nullifies the sessionStorage 'loggedInUser' item. Then shows an alert 'Logout successful!' and redirect to the homepage.
 
 function logout() {
-    currentUser = null;
-    sessionStorage.setItem('currentUser', null);
+    loggedInUser = null;
+    sessionStorage.setItem('loggedInUser', null);
     alert("Logout successful!");
     window.location.replace("index.html");
 }
 
-function setParagCurrentUser() {
-    if (location.pathname.includes("index") && currentUser != null)
-        $("#pCurrentUser").text(currentUser.name);
+// setParagloggedInUser(): set the value for the placeholder in the homepage to the name of the logged in user.
+
+function setParagloggedInUser() {
+    if (location.pathname.includes("index") && loggedInUser != null)
+        $("#ploggedInUser").text("Hello " + loggedInUser.name + "!");
 }
 
-function userAlreadyPlayed() {
-    if (location.pathname.includes("play") && currentUser != null)
-        if (!isAllowedToPlay()) {
-            $("#btnPlay").hide();
+// setPlayPage(): set the components of the 'play' page depending on if the user is allowed to play.
 
-        }
-    hideChoiceButtons();
+function setPlayPage() {
+    if (location.pathname.includes("play")) { // check if the page is 'play'
+        if (!isAllowedToPlay())  // runs the validation function isAllowedToPlay()
+            hideChoiceButtons(); // it hides the choiceButtons, to be shown only after the 'play' button is clicked
+        else
+            play();
+    }
 }
+
+// validateAndRegister(e): event fired when 'Register' button is clicked on the registration page.
 
 function validateAndRegister(e) {
-    e.preventDefault();
+    e.preventDefault(); // It prevents the page refresh
 
-    if (isUsernameFree())
-        if (isPasswordValid()) {
-            users[$("#inputEmail").val()] = createUser();
-            setItemInSessionStorage("users", users);
+    if (isUsernameFree()) // check if the chosen username is free
+        if (isPasswordValid()) { // check if the chosen password is valid.
+            users[$("#inputEmail").val()] = createUser(); // it creates a new user in the array with the registration email as array item's 'id'
+            setItemInSessionStorage("users", users); // update the 'users' array in the sessionStorage
             alert("Registration successful!");
-            window.location.replace("login.html");
+            window.location.replace("login.html"); // redirect to the login page
         }
 
 }
+
+// createUser(): it creates a new "user" item in the users array. Gets the birth date from the form input. Return an object ("user") with valued items from the form inputs.
 
 function createUser() {
     var dateOf_Birth = new Date($("#inputBirth").val());
-    dateOf_Birth.setHours(0, 0, 0, 0);
+    dateOf_Birth.setHours(0, 0, 0, 0); // set the object hours, minutes, seconds, and milliseconds to 0 for a correct comparison with the current date in the function notAlreadyPlayed().
     return {
         email: $("#inputEmail").val(),
         password: $("#inputPassword").val(),
@@ -79,120 +97,153 @@ function createUser() {
         dateOfLastPlay: "",
         attemptsLeft: 3,
         wonPrizes: 0,
-        gender: $('input[name="rGender"]:checked').val()
+        gender: $('input[name="rGender"]:checked').val() // get the value from the checked radio input for gender.
     }
 }
 
+// isUsernameFree(): returns if the username/email chosen during the registration is not already taken by another previous user.
+
 function isUsernameFree() {
 
-    if (!jQuery.isEmptyObject(users))
-        if (typeof users[$("#inputEmail").val()] !== 'undefined') {
-            $("#userErrorplaceholder").text("Username already in use! Chose another one.");
+    if (!jQuery.isEmptyObject(users))   // check if any user exist by checking if the users array is empty
+        if (typeof users[$("#inputEmail").val()] !== 'undefined') {     // check if the chosen username/email is taken by verifying if an element in the array with the chosen username/email as id exist.
+            $("#userErrorplaceholder").text("Username already in use! Chose another one."); // valorize the placeholder with the error message.
             return false;
         }
     return true;
 }
 
+// isPasswordValid(): return if the chosen password in the registration from respects the given parameters. Regular expressions are used for checking string content.
+
 function isPasswordValid() {
     var isvalid = true;
+    resetLiPasswordRequirementColor(); // reset pwd requirement list color
     var pwd = $("#inputPassword").val();
 
-    var errorMessage = "";
-    if (pwd.length < 8 || pwd.length > 18) {
-        errorMessage += "The password is not between 8 and 18 characters.\n"
+    // if any of the conditions is not respected, it sets the boolean variable isValid to false. 
+    // It colors the unrespected requirement message to red to highlight the missing requirement to the user.                   
+
+    if (pwd.length < 8 || pwd.length > 18) {    // check pwd lenght
+        $("#liLenght").addClass("password_requirement_missing");
         isvalid = false;
     }
 
     var pattern = /[0-9]/g;
-    if (!pwd.match(pattern)) {
-        errorMessage += "The password doesn't contain at least a number.\n"
+    if (!pwd.match(pattern)) {      // check if pwd contains at least one number
+        $("#liNumber").addClass("password_requirement_missing");
         isvalid = false;
     }
 
     pattern = /[a-z]/g;
-    if (!pwd.match(pattern)) {
-        errorMessage += "The password doesn't contain at least a lower case letter.\n"
+    if (!pwd.match(pattern)) {      // check if pwd contains at least one lowercase character
+        $("#liLowerChar").addClass("password_requirement_missing");
         isvalid = false;
     }
 
     pattern = /[A-Z]/g;
-    if (!pwd.match(pattern)) {
-        errorMessage += "The password doesn't contain at least an upper case letter.\n"
+    if (!pwd.match(pattern)) {      // check if pwd contains at least one uppercase character
+        $("#liUpperChar").addClass("password_requirement_missing");
         isvalid = false;
     }
 
     pattern = /[@#$%^&-+=()]/g;
-    if (!pwd.match(pattern)) {
-        errorMessage += "The password doesn't contain one at least a special character @#$%^&-+=()."
+    if (!pwd.match(pattern)) {      // check if pwd contains at least one special character
+        $("#liSpecialChar").addClass("password_requirement_missing");
         isvalid = false;
     }
-    if (!isvalid)
-        $("#pwdErrorplaceholder").text(errorMessage);
 
     return isvalid;
 }
 
+// resetLiPasswordRequirementColor(): remove class .password_requirement_missing from the li element of the password requirement list #pwdRequirements in the signup page.
+
+function resetLiPasswordRequirementColor() {
+    $("#liLenght").removeClass("password_requirement_missing");
+    $("#liNumber").removeClass("password_requirement_missing");
+    $("#liUpperChar").removeClass("password_requirement_missing");
+    $("#liLowerChar").removeClass("password_requirement_missing");
+    $("#liSpecialChar").removeClass("password_requirement_missing");
+}
+
+// login(e): event fired by clicking on the 'btnLogin' in the login page.
+
 function login(e) {
     e.preventDefault();
+    // get the values from the input form
     var email = $("#inputEmail").val();
     var pwd = $("#inputPassword").val();
 
-    if (typeof users[email] === 'undefined')
+    if (typeof users[email] === 'undefined')    // check if a user item with the login email as id exist. If not, return error in the placeholder.
         $("#userErrorplaceholder").text("This email is not registred.");
     else
-        if (users[email].password !== pwd)
+        if (users[email].password !== pwd)  // check if the login password matches the one registered in the user object with the login email as id in the users array. If not, return error in the placeholder.
             $("#pwdErrorplaceholder").text("This password is incorrect.");
         else {
-            currentUser = users[email];
-            updateSessionCurrentUser();
+            // if user and pwd are correct, set the loggedInUser as the logged in user, update the loggedInUser in the sessionStorage, show an alert with 'Login Successful!', and redirect to the homepage.
+            loggedInUser = users[email];
+            updateSessionloggedInUser();
             alert("Login Successful!");
             window.location.replace("index.html");
         }
 }
 
+// setItemInSessionStorage(key, value): set the value of 'key' item in sessionStorage at 'value'. Uses JSON to stringify the value.
+
 function setItemInSessionStorage(key, value) {
     sessionStorage.setItem(key, JSON.stringify(value));
 }
 
-function play(e) {
-    //$("#placeholder").text(extractCoupons());
-    $("#btnPlay").hide();
-    setCouponLocation();
-    $("#placeholder").text("Guess under which card is the coupon! You have " + currentUser.attemptsLeft + " attempts for today.");
-    showChoiceButtons();
+// play(e): event fired by clicking on the 'btnPlay' in the play page.      
+
+function play() {
+
+    setCouponLocation(); // set the coupon location for the current play round
+    $("#placeholder").text("Guess under which card is the coupon! You have " + loggedInUser.attemptsLeft + " attempts for today."); // shows the instructions for playing and the attempts left in a placeholder (field in loggedInUser)
+
 }
+
+// checkResult(e): event fired by clicking on the coupon cards. Check if the clicked card is the one associated with the coupon or not and inform the user.
 
 function checkResult(e) {
     var choice = $(e.target).attr('choiceNum');
     if (choice == couponLocation) {
         $("#placeholder").text("You found the coupon!");
-        currentUser.wonPrizes++;
+        loggedInUser.wonPrizes++;
     } else {
         $("#placeholder").text("The coupon was under card " + (couponLocation + 1) + ".");
     }
 
-    currentUser.attemptsLeft--;
-    $("#placeholder").append(" Attempts left for today: " + currentUser.attemptsLeft);
+    // Reduce the attempts left for the day on the loggedInUser object and inform the user.
+    loggedInUser.attemptsLeft--;
+    $("#placeholder").append(" Attempts left for today: " + loggedInUser.attemptsLeft);
 
-
-
-    if (currentUser.attemptsLeft == 0)
+    // If there is no attempt left, calls the actionsWhenAttemptsFinish() function.
+    if (loggedInUser.attemptsLeft == 0)
         actionsWhenAttemptsFinish();
 
-    updateSessionCurrentUser();
+    // Call the methods for updating sessionStorage users and loggedInUser objects.
+    updateSessionloggedInUser();
     updateSessionUserlist();
 
+    // Set new coupon location for the next round.
     setCouponLocation();
 }
 
+// actionsWhenAttemptsFinish(): actions when the user has no more guessing attempts at finding the coupons.
+
 function actionsWhenAttemptsFinish() {
-    setDateOfLastPlayToToday();
+    setDateOfLastPlayToToday(); // set the day of last play to loggedInUser. 
     $("#placeholder").append("<br></br><b>It was your last attempt for today! Come back tomorrow to play again.</b> " +
-        "<br></br>");
-    if (currentUser.wonPrizes > 0)
+        "<br></br>"); // Informs the user it was the last attempt. 
+
+    // If the user won any prizes, extract the coupons and inform the user.
+    if (loggedInUser.wonPrizes > 0)
         $("#placeholder").append("Here are the coupons you won today: " + extractCoupons());
+    // Hide choicebuttons to prevent further play.
     hideChoiceButtons();
 }
+
+// showChoiceButtons(): shows the choice buttons (cards)
 
 function showChoiceButtons() {
     $("#btnChoice1").show();
@@ -200,35 +251,48 @@ function showChoiceButtons() {
     $("#btnChoice3").show();
 }
 
+// hideChoiceButtons(): hide the choice buttons (cards)
+
 function hideChoiceButtons() {
     $("#btnChoice1").hide();
     $("#btnChoice2").hide();
     $("#btnChoice3").hide();
 }
 
+// setCouponLocation(): set variable couponLocation to a randomly generated number.
+
 function setCouponLocation() {
-    couponLocation = randomNumberGen(1);
+    couponLocation = randomNumberGen();
 }
+
+// isAllowedToPlay(): return the result of the validation of the player attempt to play.
 
 function isAllowedToPlay() {
     return isLoggedIn() && notAlreadyPlayed() && isOver18();
 }
 
+// notAlreadyPlayed(): check if the user played already on the same day.
+
 function notAlreadyPlayed() {
+    // Get the current date and set hours, minutes, seconds, and milliseconds to 0 to allow comparison with user's date of last play.
     var currentDate = new Date();
     currentDate.setHours(0, 0, 0, 0);
-    var dateOfLastPlay = new Date(currentUser.dateOfLastPlay);
+    // Get user's date of last play.
+    var dateOfLastPlay = new Date(loggedInUser.dateOfLastPlay);
+    // Check if the dates are the same. If so, return error message, otherwise resets the attemptLeft counter to 3.
     if (currentDate.getTime() == dateOfLastPlay.getTime()) {
         $("#placeholder").text("You played already today! Come back tomorrow.");
         return false;
     } else {
-        currentUser.attemptsLeft = 3;
+        loggedInUser.attemptsLeft = 3;
         return true;
     }
 }
 
+// isLoggedIn(): check if there is a logged in user verifying that loggedInUser is not null. If null, shows an error alert and redirect to the login page.
+
 function isLoggedIn() {
-    if (currentUser == null) {
+    if (loggedInUser == null) {
         alert("You need to log in first.");
         window.location.replace("login.html");
         return false;
@@ -237,11 +301,15 @@ function isLoggedIn() {
     }
 }
 
+// isOver18(): check if the user is over 18.
+
 function isOver18() {
-    var birthDate = new Date(currentUser.dateOfBirth);
+    // Get user's birth date.
+    var birthDate = new Date(loggedInUser.dateOfBirth);
     var currentDate = new Date();
 
-    //31556952000 is the number of milliseconds in an year
+    // Check if the difference between today's date and the user's birth date is over 18 years; if not, shows an error message. 
+    // 31556952000 is the number of milliseconds in an year
     if ((currentDate - birthDate) / 31556952000 >= 18)
         return true;
     else {
@@ -250,24 +318,32 @@ function isOver18() {
     }
 }
 
+// extractCoupons(): extract a coupon for each prize won by the user.
+
 function extractCoupons() {
     var prizeMessage = "";
 
-    for (var i = 0; i < currentUser.wonPrizes; i++) {
+    for (var i = 0; i < loggedInUser.wonPrizes; i++) {
         prizeMessage += assignPrize(randomNumberGen());
     }
     return prizeMessage;
 }
 
+// setDateOfLastPlayToToday(): set the date of last play to today to the loggedInUser.
+
 function setDateOfLastPlayToToday() {
     let currentDate = new Date();
     currentDate.setHours(0, 0, 0, 0);
-    currentUser.dateOfLastPlay = currentDate;
+    loggedInUser.dateOfLastPlay = currentDate;
 }
+
+// randomNumberGen(): generates a random number between 0 and 2.
 
 function randomNumberGen() {
     return Math.floor(Math.random(3) * 3);
 }
+
+// assignPrize(extractedNum): return a message with the coupon extracted given a number as parameter.
 
 function assignPrize(extractedNum) {
 
@@ -284,11 +360,54 @@ function assignPrize(extractedNum) {
     }
 }
 
+// updateSessionUserlist(): updates the sessionStorage 'users' array with the current 'users' object.
+
 function updateSessionUserlist() {
-    users[currentUser.email] = currentUser;
+    users[loggedInUser.email] = loggedInUser;
     setItemInSessionStorage('users', users);
 }
 
-function updateSessionCurrentUser() {
-    setItemInSessionStorage('currentUser', currentUser);
+// updateSessionloggedInUser(): updates the sessionStorage 'loggedInUser' object with the current 'loggedInUser' object.
+
+function updateSessionloggedInUser() {
+    setItemInSessionStorage('loggedInUser', loggedInUser);
 }
+
+
+
+let items = document.querySelectorAll('.slider .item');
+    let next = document.getElementById('next');
+    let prev = document.getElementById('prev');
+    
+    let active = 1;
+    function loadShow(){
+        let stt = 0;
+        items[active].style.transform = `none`;
+        items[active].style.zIndex = 1;
+        items[active].style.filter = 'none';
+        items[active].style.opacity = 1;
+        for(var i = active + 1; i < items.length; i++){
+            stt++;
+            items[i].style.transform = `translateX(${120*stt}px) scale(${1 - 0.2*stt}) perspective(16px) rotateY(-1deg)`;
+            items[i].style.zIndex = -stt;
+            items[i].style.filter = 'blur(5px)';
+            items[i].style.opacity = stt > 2 ? 0 : 0.6;
+        }
+        stt = 0;
+        for(var i = active - 1; i >= 0; i--){
+            stt++;
+            items[i].style.transform = `translateX(${-120*stt}px) scale(${1 - 0.2*stt}) perspective(16px) rotateY(1deg)`;
+            items[i].style.zIndex = -stt;
+            items[i].style.filter = 'blur(5px)';
+            items[i].style.opacity = stt > 2 ? 0 : 0.6;
+        }
+    }
+    loadShow();
+    next.onclick = function(){
+        active = active + 1 < items.length ? active + 1 : active;
+        loadShow();
+    }
+    prev.onclick = function(){
+        active = active - 1 >= 0 ? active - 1 : active;
+        loadShow();
+    }
